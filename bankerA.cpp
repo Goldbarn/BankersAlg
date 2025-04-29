3971ec3a5549f629c383c84ceafe102ea97ccee9
Goldbarn <152343413+Goldbarn@users.noreply.github.com>
2025-04-28 20:44:37 -0400

    did algorithm

--- bankerA.cpp
+++ bankerA.cpp
@@ -5,20 +5,52 @@ const int R = 3; //num of resources
 
 bool isSafe(int proc[], int max[][R], int alloc[][R], int avail[]) {
 
-    int need[P][R]; //Preppin need array
+    int need[P][R]; // Prepping need array
 
     // Calculating need 
     for (int i = 0 ; i < P ; i++)
         for (int j = 0 ; j < R ; j++)
-
-            // Need of instance = maxm instance - allocated instance
             need[i][j] = max[i][j] - alloc[i][j];
-
 
     bool done[P] = {false}; // Mark all processes as not finished
     int safeSeq[P]; // Store safe sequence
+    int work[R]; // Work array
 
+    for (int i = 0; i < R; i++)
+        work[i] = avail[i]; // Initialize work as available resources
 
+    int count = 0; // Count of processes in safe sequence
+    
+    while (count < P) { // While all processes are not finished
+        bool found = false; // Flag to check if process is found
+        for (int p = 0; p < P; p++) { // Check each process
+            if (!done[p]) { // If process is not finished
+                int j;
+                for (j = 0; j < R; j++) // Check if need <= work
+                    if (need[p][j] > work[j])
+                        break;
+
+                if (j == R) { // If all resources are less than or equal to work
+                    for (int k = 0 ; k < R ; k++)
+                        work[k] += alloc[p][k]; // Add allocated resources to work
+                    safeSeq[count++] = p; // Add process to safe sequence
+                    done[p] = true; // Mark process as finished
+                    found = true; // Process is found
+                }
+            }
+        }
+        if (!found) { // If no process is found, system is not in a safe state
+            return false;
+        }
+    }
+
+    // If we reach here, then the system is in a safe state
+    std::cout << "Safe sequence is: ";
+    for (int i = 0; i < P; i++)
+        std::cout << safeSeq[i] << " ";
+    std::cout << std::endl;
+
+    return true;
 }
 
 int main() {
@@ -41,12 +73,8 @@ int main() {
 
     int avail[R] = {3, 3, 2};
 
-    if (isSafe(proc, max, alloc, avail)) { //Check if safe, then return result
-        std::cout << "System is in a safe state." << std::endl;
-    } else {
-        std::cout << "System is not in a safe state." << std::endl;
-    }
-
+    isSafe(proc, max, alloc, avail);
+    
     return 0;
 
 }
